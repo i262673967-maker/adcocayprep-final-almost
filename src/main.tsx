a3e19c6854f666ca2061454c126004493fc1d978
i262673967-maker <i262673967@gmail.com>
2026-08-12 20:48:37 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,35 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
+import Pricing from './pages/Pricing.tsx';
+import Terms from './pages/Terms.tsx';
+import Privacy from './pages/Privacy.tsx';
+import Refunds from './pages/Refunds.tsx';
 import './index.css';
+
+const path = window.location.pathname.replace(/\/+$/, '') || '/';
+
+const Page = (() => {
+  switch (path) {
+    case '/pricing':
+      return <Pricing />;
+
+    case '/terms':
+      return <Terms />;
+
+    case '/privacy':
+      return <Privacy />;
+
+    case '/refunds':
+      return <Refunds />;
+
+    default:
+      return <App />;
+  }
+})();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    {Page}
+  </StrictMode>
 );
